6bf1552664a1435e4431206f7b7ae00eed40a2af
Roberto Gonzalez <roberto@aerolab.co>
2018-08-30 15:31:13 -0300

    Added generic wildcard cert

--- utils/letsencrypt.js
+++ utils/letsencrypt.js
@@ -5,9 +5,8 @@ chalk = require('chalk')
 function restart(dokkuApp) {
 
   let spinner = ora({ text: 'Adding Lets Encrypt to '+ chalk.bold.white(dokkuApp), spinner: 'dots' }).start();
-
   let logs = shell.exec(
-    "ssh -tt dokku@wip.aerolab.co -- letsencrypt " + dokkuApp,
+    "ssh -tt dokku@wip.aerolab.co -- certs:add "+ dokkuApp +" /home/dokku/.https/server.crt /home/dokku/.https/server.key ",
     { async:true, silent:true },
     function(code, stdout, stderr) {
       // Code 0 is success for ssh
